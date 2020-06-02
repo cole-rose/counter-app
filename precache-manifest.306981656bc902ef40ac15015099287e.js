5fcadbcc9b334cd125e72f6977754fbec0a674d4
cole-rose <colerose@berkeley.edu>
2020-06-02 13:14:27 -0700

    Updates

---
+++ precache-manifest.306981656bc902ef40ac15015099287e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f044e8625f666417b90c53e13648eaf7",
+    "url": "/cole.rose.github.io/counter-app/index.html"
+  },
+  {
+    "revision": "63962cc45c930472843b",
+    "url": "/cole.rose.github.io/counter-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "dc1cd7da6b28e2c08fb1",
+    "url": "/cole.rose.github.io/counter-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "63962cc45c930472843b",
+    "url": "/cole.rose.github.io/counter-app/static/js/2.78e75b79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cole.rose.github.io/counter-app/static/js/2.78e75b79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc1cd7da6b28e2c08fb1",
+    "url": "/cole.rose.github.io/counter-app/static/js/main.eaacda23.chunk.js"
+  },
+  {
+    "revision": "57c20acba945f206511f",
+    "url": "/cole.rose.github.io/counter-app/static/js/runtime-main.a3a2951c.js"
+  }
+]);
